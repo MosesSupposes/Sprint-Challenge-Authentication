bb29fd74946fdee139c1a2a31c7c2c6a955c74e9
Moses Samuel <mosamuel98@gmail.com>
2019-10-18 11:06:54 -0700

    style login and register forms

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,6 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
+import { Form, Button } from 'semantic-ui-react'
+import styled from 'styled-components'
+
+
+export const Container = styled.div`
+	display: flex;
+	flex-grow: 2;
+	flex-direction: column;
+	margin-top: 4rem;
+	align-items: center;
+	padding-bottom: 45vh;
+`
+
+export const Underlined = styled.span`
+	text-decoration: underline
+`	
+
+export const P = styled.p`
+	margin: 1rem 0;
+`
 
 export default function Login(props) {
     const [creds, setCreds] = React.useState({
@@ -26,26 +46,32 @@ export default function Login(props) {
     }
 
     return (
-        <div>
+        <Container>
             <h2>Login</h2>
-            <form onSubmit={handleSubmit}>
-                <input 
-                    name="username"
-                    type="text" 
-                    onChange={handleInput}
-                    value={creds.username}
-                />
+            <Form onSubmit={handleSubmit}>
+                <Form.Field>
+                    <label>Username</label>
+                    <input 
+                        name="username"
+                        type="text" 
+                        onChange={handleInput}
+                        value={creds.username}
+                    />
+                </Form.Field>
 
-                <input 
-                    name="password"
-                    type="password" 
-                    onChange={handleInput}
-                    value={creds.password}
-                />
-                <button type="submit">Login</button>
-            </form>
-
-            <Link to="/register">Not a user? Register</Link>
-        </div>
+                <Form.Field>
+                    <label>Password</label>
+                    <input 
+                        name="password"
+                        type="password" 
+                        onChange={handleInput}
+                        value={creds.password}
+                    />
+                </Form.Field>
+                
+                <Button type="submit">Login</Button>
+                <P><strong>Not a user?</strong> <Link to="/register"><Underlined>Register now.</Underlined></Link></P>
+            </Form>
+        </Container>
     )
 }
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,6 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
+import { Form, Button } from 'semantic-ui-react'
+import styled from 'styled-components'
+
+export const Container = styled.div`
+	display: flex;
+	flex-grow: 2;
+	flex-direction: column;
+	margin-top: 4rem;
+	align-items: center;
+	padding-bottom: 45vh;
+`
+
+export const Underlined = styled.span`
+	text-decoration: underline
+`	
+
+export const P = styled.p`
+	margin: 1rem 0;
+`
 
 export default function Login(props) {
     const [creds, setCreds] = React.useState({
@@ -25,25 +44,32 @@ export default function Login(props) {
     }
 
     return (
-        <div>
+        <Container>
             <h2>Register</h2>
-            <form onSubmit={handleSubmit}>
-                <input 
-                    name="username"
-                    type="text" 
-                    onChange={handleInput}
-                    value={creds.username}
-                />
+            <Form onSubmit={handleSubmit}>
+                <Form.Field>
+                    <label>Username</label>
+                    <input 
+                        name="username"
+                        type="text" 
+                        onChange={handleInput}
+                        value={creds.username}
+                    />
+                </Form.Field>
 
-                <input 
-                    name="password"
-                    type="password" 
-                    onChange={handleInput}
-                    value={creds.password}
-                />
-                <button type="submit">Register</button>
-            </form>
-            <Link to='/'>Already a user? Login</Link>
-        </div>
+                <Form.Field>
+                    <label>Password</label>
+                    <input 
+                        name="password"
+                        type="password" 
+                        onChange={handleInput}
+                        value={creds.password}
+                    />
+                </Form.Field>
+                
+                <Button type="submit">Login</Button>
+                <P><strong>Already a user?</strong> <Link to="/register"><Underlined>Login now.</Underlined></Link></P>
+            </Form>
+        </Container>
     )
 }
