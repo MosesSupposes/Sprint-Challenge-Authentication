8efac9379d39c2afa2d3b6a525281f9eec522430
Moses Samuel <mosamuel98@gmail.com>
2019-10-18 09:25:04 -0700

    fix reference error bug

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -6,7 +6,7 @@ const jwtSecret = require('../config/secrets').JWT_SECRET
 */
 
 module.exports = (req, res, next) => {
-	const { token } = req.headers.authorization
+	const token = req.headers.authorization
 
 	if (token) {
 		jwt.verify(token, jwtSecret, (err, decodedToken) => {
